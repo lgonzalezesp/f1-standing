e7f48bc2932ea62dbf440173c5f33de352a4782b
Luis Gonzalez Espejo <luis.gonzalez.espejo@gmail.com>
2025-06-03 19:06:07 -0400

    Add routing configuration and GitHub Pages deployment workflow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
 
 export const routes: Routes = [];
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation())
+  ]
+};
